273df2701f9aaf7d783377efd41356543e7221ed
ArthurCiyi <200954381@qq.com>
2020-11-25 00:19:06 +0800

    w

--- code/hello.ts
+++ code/hello.ts
@@ -1,12 +1,1 @@
-
-let sum=[200,30,123,45,0,0,0,564];
-let max  =[];
-for(let i =0; i<sum.length ;i++){
-    if(0 < sum[i]){
-        max[max.length] = sum[i]
-    }
-}
-
 console.log();
-
-console.log(max);
