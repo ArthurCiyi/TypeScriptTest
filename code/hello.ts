beb9f78e6e7db861cea52f351417a477d030aa84
ArthurCy <200954381@qq.com>
2020-11-24 22:09:15 +0800

    第二次提交

--- code/hello.ts
+++ code/hello.ts
@@ -1,6 +1,7 @@
-let sum: number[]=[200,30,123,45,0,0,0,564];
-let max :number[] =[];
-for(let i:number =0; i<sum.length ;i++){
+
+let sum=[200,30,123,45,0,0,0,564];
+let max  =[];
+for(let i =0; i<sum.length ;i++){
     if(0 < sum[i]){
         max[max.length] = sum[i]
     }
